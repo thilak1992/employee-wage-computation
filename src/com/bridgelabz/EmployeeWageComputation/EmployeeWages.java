135bbe46ff9db4176e8a9ee1ef1f6b79034b1659
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-11-09 09:20:22 +0530

    Calculating: Employee Wages Per Month

--- src/com/bridgelabz/EmployeeWageComputation/EmployeeWages.java
+++ src/com/bridgelabz/EmployeeWageComputation/EmployeeWages.java
@@ -7,24 +7,33 @@ public class EmployeeWages {
         int wagePerHour = 20;
         int fullTimeHour = 8;
         int partTimeHour = 4;
-        int dailyWage = 0;
+        int workingDays = 20;
+        int FullTimeWage = 0;
+        int PartTimewage = 0;
+        int absent = 0;
+        for (int day = 0; day < workingDays; day++) {
+            int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
 
-        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+            switch (employeeCheck) {
+                case 2:
+                    FullTimeWage = FullTimeWage + fullTimeHour * wagePerHour;
 
-        switch (employeeCheck) {
-            case 2:
-                dailyWage = fullTimeHour * wagePerHour;
-                System.out.println("Employee is present full time");
-                break;
-            case 1:
-                dailyWage = partTimeHour * wagePerHour;
-                System.out.println("Employee is present half time");
-                break;
-            default:
-                System.out.println("Employee is absent");
+                    break;
+                case 1:
+                    PartTimewage = PartTimewage + partTimeHour * wagePerHour;
+
+                    break;
+                default:
+                    absent++;
+                    System.out.println("Employee is absent ");
+            }
+
         }
+        System.out.println("Employee is absent for " + absent);
+        System.out.println("Employee full time Wages are: " + FullTimeWage);
+        System.out.println("Employee Part time Wages are: " + PartTimewage);
 
-        System.out.println("Employee total wage is: " + dailyWage);
+        System.out.println("Employee total wage for a month is: " + (FullTimeWage + PartTimewage));
     }
 
 
